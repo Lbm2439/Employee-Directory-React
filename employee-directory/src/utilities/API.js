a4a85b40a64af4f5ccc6ec0e91fdfdb25df5ae78
Rapunzel <Laurenminton19@yahoo.com>
2020-03-29 11:50:59 -0400

    added api/axios and worked on client side

---
+++ employee-directory/src/utilities/API.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+
+const ApiCall = "https://randomuser.me/api/?results=50&nat=us";
+
+
+export default {
+  search: function() {
+       return axios.get(ApiCall);
+  }
+};
