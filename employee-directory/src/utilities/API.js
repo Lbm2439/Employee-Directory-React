8602d4cf0e2cf72647c121e276a81a728cd96d62
Rapunzel <Laurenminton19@yahoo.com>
2020-03-29 13:23:45 -0400

    changed results to 10 at a time

--- employee-directory/src/utilities/API.js
+++ employee-directory/src/utilities/API.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const ApiCall = "https://randomuser.me/api/?results=50&nat=us";
+const RandomUser = "https://randomuser.me/api/?results=10&nat=us";
 
 
 export default {
   search: function() {
-       return axios.get(ApiCall);
+       return axios.get(RandomUser);
   }
 };
